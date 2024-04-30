313fd24d6c2e5231fb97cbf293bdf753f98ad632
Ioana Tiplea <ioanatiplea94@gmail.com>
2024-04-30 17:10:23 +0300

    feat: header with placeholder icon and text

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,8 @@
+<script>
+  import RoadLogo from "$lib/assets/images/road.png";
+</script>
+
+<div id="header" class="h-12 bg-primary flex items-center gap-4 px-4">
+  <img src={RoadLogo} alt="Road logo" class="h-8 w-8" />
+  <span class="text-xl font-bold">Scout-A-Route</span>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,7 @@
   // import { PUBLIC_MAPS_API_KEY } from "$env/static/public";
 
   import Footer from "$lib/components/Footer.svelte";
+  import Header from "$lib/components/Header.svelte";
   import Sidebar from "$lib/components/Sidebar.svelte";
 
   // onMount(async () => {
@@ -36,7 +37,7 @@
 </script>
 
 <div class="text-black h-screen flex flex-col">
-  <div id="header" class="h-12 bg-primary">Header</div>
+  <Header />
   <div id="content" class="grid grid-cols-4 grow">
     <Sidebar />
     <div id="map" class="col-span-3 bg-yellow-200">
