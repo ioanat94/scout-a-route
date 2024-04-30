0b265c1db8b72301dcdfa9b586550f5668359a7e
Ioana Tiplea <ioanatiplea94@gmail.com>
2024-04-30 17:17:19 +0300

    feat: header and footer tests

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -2,6 +2,6 @@
   import GithubIcon from "$lib/assets/icons/GithubIcon.svelte";
 </script>
 
-<div id="footer" class="h-12 bg-secondary flex items-center justify-center">
+<div data-testid="footer" class="h-12 bg-secondary flex items-center justify-center">
   <a href="https://github.com/ioanat94/scout-a-route" target="_blank"><GithubIcon /></a>
 </div>
